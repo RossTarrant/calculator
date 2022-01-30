455c1bcebb6b5c6a6f6e1637e598bcc1189d7274
RossTarrant <rosstarrant3@gmail.com>
2022-01-30 21:19:47 +0000

    Added basic math functionality

--- script.js
+++ script.js
@@ -24,9 +24,9 @@ function operate(operator, x, y){
             return add(x, y);
         case "-":
             return subtract(x, y);
-        case "*":
+        case "x":
             return multiply(x, y);
-        case "/":
+        case "÷":
             return divide(x, y);
     }
 }
@@ -39,21 +39,34 @@ function updateDisplay(buttonText){
     display.textContent = displayText;
 }
 
+
 function buttonHandler(buttonText){
     let nums = [0,1,2,3,4,5,6,7,8,9]
     let operators = ["="]
     if(buttonText in nums){
-        updateDisplay(buttonText)
+        updateDisplay(buttonText);
     }
     else if(buttonText==="+" || buttonText==="-" || buttonText==="x" || buttonText==="÷"){
+        currentOperator = buttonText;
+        if(whichNum==="left"){
+            let display = document.querySelector(".display");
+            leftNum = parseInt(display.textContent);
+            whichNum==="right";
+        }
         updateDisplay(buttonText)
     }
     else if(buttonText==="="){
-        // calculate number
+        let display = document.querySelector(".display");
+        rightNum = parseInt(display.textContent.split(currentOperator)[1]);
+        display.textContent = operate(currentOperator, leftNum, rightNum);
     }
 }
 
-let operator = "";
+let leftNum = 0;
+let rightNum = 0;
+let whichNum = "left";
+let currentOperator = "";
+
 const buttonsDiv = document.querySelector(".calc-buttons");
 const buttons = buttonsDiv.querySelectorAll("button");
 buttons.forEach(button => {
